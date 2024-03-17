ac0033245c5f10a2f26f8dce02093dcb83986b29
박권익 <kinowk@naver.com>
2024-03-17 12:37:02 +0900

    Initial commit

---
+++ src/main/java/hellp/upload/domain/Item.java
@@ -0,0 +1,16 @@
+package hellp.upload.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private UploadFile attachFile;
+    private List<UploadFile> imageFiles;
+}
